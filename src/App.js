cb9ef3d1d3e97bb8133e37fb3a154ee8dbcf1521
prateek <97218249+PrateekTomar000@users.noreply.github.com>
2022-06-30 20:37:14 +0530

    A simple Todo-lists website
    we can add or delete our task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './mycomponents/Header';
+import { Todos } from "./mycomponents/Todos";
+import Footer from './mycomponents/Footer';
+import React, {useState} from 'react';
+import { AddTodo } from './mycomponents/AddTodo';
+import { useEffect } from 'react';
 
 function App() {
+  let initTodo;
+  if(localStorage.getItem("todos")=== null) {
+     initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+   
+
+  const onDelete = (todo)=> {
+    console.log('i am on delete of todo',todo);
+
+    setTodo(todos.filter((e)=> {
+      return e!==todo;
+    }))
+    localStorage.setItem("todos", JSON.stringify(todos));  
+  }
+
+  const addTodo = (title, desc) => {
+      console.log("i am adding todo", title, desc);
+      let sno;
+      if(todos.length===0) {
+        sno = 1;
+      }
+      else {
+      sno = todos[todos.length-1].sno + 1;
+      }
+      const mytodo = {
+        sno: sno,
+        title: title,
+        desc: desc
+      }
+      setTodo([...todos, mytodo]);
+      console.log(mytodo);   
+  }
+  const [todos, setTodo ] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header title ="My Todos list" searchBar ={false} />
+     <AddTodo addTodo={addTodo} />
+     <Todos todos={todos} onDelete={onDelete} />
+     <Footer/>
+    </>
+    
   );
 }
 
---
+++ src/mycomponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>this is about section
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis dicta eligendi maxime assumenda!
+            Perferendis, nostrum tempora adipisci esse necessitatibus quam, quos at odio, consequatur eligendi
+            pariatur? Est sint doloremque eaque praesentium quo commodi fugit?
+        </p>
+    </div>
+  )
+}
